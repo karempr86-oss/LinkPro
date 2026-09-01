fa237c02451aabc06bd0c888b64a9b9a4d01596c
karempr86-oss <karempr86@gmail.com>
2026-09-01 03:10:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,5 @@ def scan_file():
     return jsonify({"status": status, "message": message})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+      import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
